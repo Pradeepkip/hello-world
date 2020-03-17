3ee2d365578dd976fd3d641131d2a0943ed7d4bd
Pradeep Kumar <pradeep.kip@gmail.com>
2020-03-16 21:45:50 -0400

    Adding dev changes

--- hello.java
+++ hello.java
@@ -2,7 +2,7 @@ public class AddTwoNumbers {
 
    public static void main(String[] args) {
         
-      int num1 = 8, num2 = 15, sum;
+      int num1 = 9, num2 = 16, sum;
       sum = num1 + num2;
 
       System.out.println("These are the Sum of numbers: "+sum);
